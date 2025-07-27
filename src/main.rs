90e5bb3c7e42fedad277c03767fd341fef7b6fbe
matttm <matttmaloney@gmail.com>
2025-07-26 22:49:19 -0400

    creating empty handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 mod queries;
+    mod models;
 
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    routing::{get, post},
+};
 use queries::{CREATE_TABLE_ACCOUNT, CREATE_TABLE_TRANSACTION, CREATE_TABLE_USER};
 use rusqlite::{Connection, Result};
 
-fn main() -> Result<()> {
+async fn get_users() -> Vec<User> {
+    vec![]
+}
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
     let conn = Connection::open_in_memory()?;
     conn.execute(CREATE_TABLE_USER, ());
     conn.execute(CREATE_TABLE_ACCOUNT, ());
     conn.execute(CREATE_TABLE_TRANSACTION, ());
-    Ok(())
+
+    let user_router = Router::new()
+	.route("", method_router)
+    // build our application with a route
+    let app = Router::new()
+	.nest("/users", router)
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
