1eef9e0dda14fc0dcafed21bccd95ae1cea25566
matttm <matttmaloney@gmail.com>
2025-07-28 20:16:00 -0400

    adding debug macro

--- src/handlers/user_handlers.rs
+++ src/handlers/user_handlers.rs
@@ -5,11 +5,13 @@ use std::sync::{Arc, Mutex};
 
 type Db = Arc<Mutex<rusqlite::Connection>>;
 
+#[axum::debug_handler]
 pub async fn get_users(State(db): State<Db>) -> Json<Vec<models::user::User>> {
     tracing::info!("Invocation to `get_users`");
     let res = services::user_service::get_users(db).await;
     Json(res.unwrap())
 }
+#[axum::debug_handler]
 pub async fn create_user(
     State(db): State<Db>,
     user: Json<models::user::UserCreation>,
