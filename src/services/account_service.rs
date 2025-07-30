839b3152ee123092ca4299115c07395a61c38b26
matttm <matttmaloney@gmail.com>
2025-07-30 16:53:12 -0400

    cleanup

--- src/services/account_service.rs
+++ src/services/account_service.rs
@@ -1,5 +1,4 @@
 use crate::models;
-use axum::{Json, extract::State};
 use std::{
     str::FromStr,
     sync::{Arc, Mutex},
@@ -9,12 +8,12 @@ use crate::services::generation_service;
 
 type Db = Arc<Mutex<rusqlite::Connection>>;
 
-pub async fn get_accounts(db: Db) -> Result<Vec<models::account::AccountGeneral>, rusqlite::Error> {
+pub async fn get_accounts(
+    db: Db,
+) -> Result<Vec<models::account::AccountGeneral>, Box<dyn std::error::Error>> {
     tracing::info!("Invocation to `get_accounts`");
-    let conn = db.lock().unwrap(); // make generic error?
-    let mut stmt = conn
-        .prepare("SELECT account_number, user_id, created_at FROM ACCOUNTS;")
-        .unwrap();
+    let conn = db.lock().unwrap();
+    let mut stmt = conn.prepare("SELECT account_number, user_id, created_at FROM ACCOUNTS;")?;
     let mut raw = stmt.query(())?;
     let mut res: Vec<models::account::AccountGeneral> = vec![];
     while let Some(row) = raw.next()? {
@@ -29,19 +28,14 @@ pub async fn get_accounts(db: Db) -> Result<Vec<models::account::AccountGeneral>
 pub async fn create_account(
     db: Db,
     account_creation: models::account::AccountCreation,
-) -> Result<models::account::AccountCreation, rusqlite::Error> {
+) -> Result<models::account::AccountCreation, Box<dyn std::error::Error>> {
     tracing::info!("Invocation to `create_account`");
     let conn = db.lock().unwrap();
     let user_id = account_creation.user_id;
     let account_number = generation_service::generate_numeric_string(20); // TODO: MAKE ENV
     conn.execute(
         "INSERT INTO ACCOUNTS (account_number, user_id, balance) VALUES (?, ?, ?);",
-        [
-            account_number,
-            user_id.to_string(),
-            String::from_str("0").unwrap(),
-        ],
-    )
-    .unwrap();
+        [account_number, user_id.to_string(), String::from_str("0")?],
+    )?;
     Ok(account_creation)
 }
--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,5 +1,4 @@
 use crate::models;
-use axum::{Json, extract::State};
 use std::sync::{Arc, Mutex};
 
 type Db = Arc<Mutex<rusqlite::Connection>>;
@@ -7,9 +6,7 @@ type Db = Arc<Mutex<rusqlite::Connection>>;
 pub async fn get_users(db: Db) -> Result<Vec<models::user::User>, rusqlite::Error> {
     tracing::info!("Invocation to `get_users`");
     let conn = db.lock().unwrap();
-    let mut stmt = conn
-        .prepare("SELECT id, username, created_at, updated_at FROM USERS;")
-        .unwrap();
+    let mut stmt = conn.prepare("SELECT id, username, created_at, updated_at FROM USERS;")?;
     let mut raw = stmt.query(())?;
     let mut res: Vec<models::user::User> = vec![];
     while let Some(row) = raw.next()? {
@@ -28,7 +25,7 @@ pub async fn create_user(
 ) -> Result<models::user::UserCreation, rusqlite::Error> {
     tracing::info!("Invocation to `create_user`");
     let conn = db.lock().unwrap();
-    let res = conn.execute(
+    conn.execute(
         "INSERT INTO USERS (username, password) VALUES (?, ?);",
         [user.username.as_str(), user.password.as_str()],
     )?;
