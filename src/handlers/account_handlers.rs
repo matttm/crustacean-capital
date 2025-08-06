1857ccf6965b176c2d21f0c336a079659cf62171
matttm <matttmaloney@gmail.com>
2025-08-06 08:41:31 -0400

    migrating tx to sqlx

--- src/handlers/account_handlers.rs
+++ src/handlers/account_handlers.rs
@@ -1,23 +1,22 @@
 use crate::models;
 use crate::services;
 use axum::{Json, extract::State};
-use std::sync::Arc;
-use tokio::sync::Mutex;
-
-type Db = Arc<Mutex<rusqlite::Connection>>;
+use sqlx::SqlitePool;
 
 #[axum::debug_handler]
-pub async fn get_accounts(State(db): State<Db>) -> Json<Vec<models::account::AccountGeneral>> {
+pub async fn get_accounts(
+    State(db): State<SqlitePool>,
+) -> Json<Vec<models::account::AccountGeneral>> {
     tracing::info!("Invocation to `get_accounts`");
-    let res = services::account_service::get_accounts(db).await;
+    let res = services::account_service::get_accounts(&db).await;
     Json(res.unwrap())
 }
 #[axum::debug_handler]
 pub async fn create_account(
-    State(db): State<Db>,
+    State(db): State<SqlitePool>,
     account: Json<models::account::AccountCreation>,
 ) -> Json<models::account::AccountGeneral> {
     tracing::info!("Invocation to `create_accounts`");
-    let res = services::account_service::create_account(db, account.0).await;
+    let res = services::account_service::create_account(&db, account.0).await;
     Json(res.unwrap())
 }
--- src/services/transaction_service.rs
+++ src/services/transaction_service.rs
@@ -1,60 +1,48 @@
+use sqlx::SqlitePool;
+
 use crate::models;
 use crate::models::transaction::{TransactionCreation, TransactionGeneral};
-use rusqlite::Connection;
 use std::{str::FromStr, sync::Arc};
-use tokio::sync::Mutex;
-
-type Db = Arc<Mutex<rusqlite::Connection>>;
 
 pub async fn get_transactions(
-    db: Db,
+    db: &SqlitePool,
 ) -> Result<Vec<models::transaction::TransactionGeneral>, Box<dyn std::error::Error>> {
     tracing::info!("Invocation to `get_transactions`");
-    let conn = db.lock().await;
-    let mut stmt = conn.prepare("SELECT id, account_number, seller, amount FROM TRANSACTIONS;")?;
-    let mut raw = stmt.query(())?;
-    let mut res: Vec<models::transaction::TransactionGeneral> = vec![];
-    while let Some(row) = raw.next()? {
-        res.push(models::transaction::TransactionGeneral {
-            id: row.get(0)?,
-            account_number: row.get(1)?,
-            seller: row.get(2)?,
-            amount: row.get(3)?,
-        })
-    }
-    Ok(res)
+    let transactions: Vec<models::transaction::TransactionGeneral> =
+        sqlx::query_as("SELECT id, account_number, seller, amount FROM TRANSACTIONS;")
+            .fetch_all(db)
+            .await?;
+    Ok(transactions)
 }
 pub async fn create_transaction(
-    db: Db,
+    db: &SqlitePool,
     transaction_creation: models::transaction::TransactionCreation,
 ) -> Result<models::transaction::TransactionCreation, Box<dyn std::error::Error>> {
     tracing::info!("Invocation to `create_transaction`");
-    let mut conn = db.lock().await;
-    let tx = conn.transaction()?;
+    let mut tx = db.begin().await?;
     // get account and checck balance
     let account_number = transaction_creation.account_number.to_string();
-    let mut balance: f32 = tx.query_one(
-        "SELECT id, balance FROM ACCOUNTS;",
-        [&account_number],
-        |row| row.get(1),
-    )?;
+    let mut balance: Option<f32> =
+        sqlx::query_scalar("SELECT balance FROM ACCOUNTS WHERE account_number = ?;")
+            .bind(&account_number)
+            .fetch_one(&mut *tx)
+            .await?;
     if transaction_creation.amount > balance {
         return Err("Insufficient funds".into());
     }
-    tx.execute(
-        "INSERT INTO TRANSACTIONS (account_number, seller, amount) VALUES (?, ?, ?);",
-        [
-            &account_number,
-            &transaction_creation.seller,
-            &transaction_creation.amount.to_string(),
-        ],
-    )?;
+    sqlx::query("INSERT INTO TRANSACTIONS (account_number, seller, amount) VALUES (?, ?, ?);")
+        .bind(&account_number)
+        .bind(&transaction_creation.seller)
+        .bind(&transaction_creation.amount.to_string())
+        .execute(&mut *tx)
+        .await?;
     balance -= transaction_creation.amount;
-    tx.execute(
-        "UPDATE ACCOUNTS SET balance WHERE account_number = ?;",
-        [balance.to_string(), account_number],
-    )?;
-    tx.commit()?;
+    sqlx::query("UPDATE ACCOUNTS SET balance WHERE account_number = ?;")
+        .bind(balance.to_string())
+        .bind(account_number)
+        .execute(&mut *tx)
+        .await?;
+    tx.commit().await?;
     Ok(transaction_creation)
 }
 
@@ -67,7 +55,7 @@ mod tests {
 
     use super::*;
 
-    fn setup_db() -> Db {
+    fn setup_db() -> DbSqlitePool {
         let conn = Connection::open_in_memory().unwrap();
         conn.execute(queries::CREATE_TABLE_USER, []).unwrap();
         conn.execute(queries::CREATE_TABLE_ACCOUNT, []).unwrap();
